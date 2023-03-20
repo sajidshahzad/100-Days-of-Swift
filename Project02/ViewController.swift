2de70c040665b1aacec47f34a00e2e9442b10432
sajidshahzad <sajidshahzad266@gmail.com>
2023-03-20 15:00:54 +0500

    Initial Commit

---
+++ Project02/ViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Project02
+//
+//  Created by Sajid Shahzad on 24/02/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button1: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswer = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        askQuestion()
+    }
+    
+    func askQuestion(action: UIAlertAction! = nil ){
+        
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = countries[correctAnswer].uppercased()
+    }
+
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        
+        if sender.tag == correctAnswer{
+            title = "Correct Answer"
+            score += 1
+        }else {
+            title = "Wrong Answer"
+            score -= 1
+        }
+        
+        let ac =  UIAlertController(title: title, message: "Your Score is \(score)", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+        
+        present(ac, animated: true)
+    }
+    
+}
+
